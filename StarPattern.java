55a6efc18f70dfb523d62f554707a3f7a4840b86
Aniket <144583684+AniketMali2005@users.noreply.github.com>
2023-10-06 02:54:35 +0530

    Completed StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -1,9 +1,15 @@
 //Write a program to print the pattern of stars
 public class StarPattern {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         pattern();
     }
-    public static void pattern(){
 
+    public static void pattern() {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
